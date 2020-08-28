5710375659e3e378f9c074293db26ddeba166598
Rajeev B R <rajeevsuchiha@gmail.com>
2020-08-28 23:58:44 +0530

    Added: Seq user generate list tuple file added

---
+++ Python Basics/SeqCsnLstTpl.py
@@ -0,0 +1,10 @@
+values = input("Enter comma seprated numbers : ")
+
+#seperate input values by comma
+list = values.split(",")
+
+#convert list into tuple
+tuple = tuple(list)
+
+print("List : ",list)
+print("Tuple : ",tuple)
