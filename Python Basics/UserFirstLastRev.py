3775e2a48d60806693e45ca949df7191161a498a
Rajeev B R <rajeevsuchiha@gmail.com>
2020-08-28 23:33:33 +0530

    Added: User first last name reverse file added.

---
+++ Python Basics/UserFirstLastRev.py
@@ -0,0 +1,5 @@
+firstName = input(str(print("Enter your first name : ")))
+firstName = firstName[::-1]
+lastName = input(str(print("Enter your last name : ")))
+lastName = lastName[::-1]
+print(lastName + " " + firstName)
